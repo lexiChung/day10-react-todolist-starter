66e4a5f55848727c23d185e510d4b4e34254906a
Lexi Zhong <zhongle5@oocl.com>
2025-09-12 15:02:41 +0800

    add todoList

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,19 @@
+import {TodoContext} from "../contexts/TodoContext";
+import {useContext} from "react";
+import {todoReducer} from "../reducers/todoReducer";
+
 const TodoList = () => {
-  return (
-      <div>This is the TodoList Component.</div>
-  );
+    const {state, dispatch} = useContext(TodoContext);
+    return (
+        <div>
+            <div>This is the TodoList Component.</div>
+            {
+                state.map(todo => {
+                    return <div>{todo.text}</div>
+                })
+            }
+        </div>
+    );
 }
 
 export default TodoList
