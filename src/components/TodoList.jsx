dd2e56364a35faeca38841a2e91f8f98d7cf4523
Lexi Zhong <zhongle5@oocl.com>
2025-09-14 13:57:20 +0800

    feat:  Click delete button can delete the todo item

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,21 +1,28 @@
 import {TodoContext} from "../contexts/TodoContext";
-import {useContext} from "react";
+import {useContext, useState} from "react";
 import "./TodoList.css"
 
 const TodoList = () => {
     const {state, dispatch} = useContext(TodoContext);
 
     function toggleDone(id) {
-        const action ={type:'DONE',id: id} ;
+        const action = {type: 'DONE', id: id};
         dispatch(action)
+    }
+
+    function deleteTodo(id) {
+        dispatch({type: 'DELETE', id});
     }
 
     return <div className={'todo-group'}>
         <div>
             <div>This is the TodoList Component.</div>
             {
-                state.map(({id,text,done}) =>{
-                    return <div className={`todo-item ${done?'done':''}`} onClick={() => toggleDone(id)}>{text}</div>
+                state.map(({id, text, done}) => {
+                    return <div className={`todo-item ${done ? 'done' : ''}`} key={id}>
+                        <span onClick={() => toggleDone(id)} style={{cursor: 'pointer'}}>{text}</span>
+                        <button className="delete-btn" onClick={() => deleteTodo(id)}>X</button>
+                    </div>
                 })
             }
         </div>
--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -1,19 +1,21 @@
 export const initialState = [
-  {id: 1, text: "the first todo", done: false},
-  {id: 2, text: "the second todo", done: false},
+    {id: 1, text: "the first todo", done: false},
+    {id: 2, text: "the second todo", done: false},
 ];
 
 // reducer is a pure function that define and gather all state update logic
 export const todoReducer = (state, action) => {
-  switch (action.type) {
-    case 'DONE':
-        return state.map(todo =>{
-                if(todo.id === action.id){
+    switch (action.type) {
+        case 'DONE':
+            return state.map(todo => {
+                if (todo.id === action.id) {
                     return {...todo, done: !todo.done};
                 }
                 return todo;
-        });
-    default:
-        return state;
-  }
+            });
+        case 'DELETE':
+            return state.filter(todo => todo.id !== action.id);
+        default:
+            return state;
+    }
 };
