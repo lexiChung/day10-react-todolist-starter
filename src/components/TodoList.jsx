c05e810d47756ada361e70f398abee13a62944b7
Lexi Zhong <zhongle5@oocl.com>
2025-09-14 13:21:36 +0800

    feat: Click todo text can make it done with line through, click again to make it undone

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,19 +1,25 @@
 import {TodoContext} from "../contexts/TodoContext";
 import {useContext} from "react";
-import {todoReducer} from "../reducers/todoReducer";
+import "./TodoList.css"
 
 const TodoList = () => {
     const {state, dispatch} = useContext(TodoContext);
-    return (
+
+    function toggleDone(id) {
+        const action ={type:'DONE',id: id} ;
+        dispatch(action)
+    }
+
+    return <div className={'todo-group'}>
         <div>
             <div>This is the TodoList Component.</div>
             {
-                state.map(todo => {
-                    return <div>{todo.text}</div>
+                state.map(({id,text,done}) =>{
+                    return <div className={`todo-item ${done?'done':''}`} onClick={() => toggleDone(id)}>{text}</div>
                 })
             }
         </div>
-    );
+    </div>
 }
 
 export default TodoList
